97c7690674667cdc25bd14758192735b77de4d92
Wiiliam D C Santana <william.santana@provider-it.com.br>
2018-10-09 09:50:20 -0300

    Menor preco completo

--- src/al/menorPreco/MenorPreco.java
+++ src/al/menorPreco/MenorPreco.java
@@ -4,23 +4,24 @@ public class MenorPreco {
 
 	public static void main(String[] args) {
 
-		double[] precos = new double[5];
-		precos[0] = 1000000;
-		precos[1] = 46000;
-		precos[2] = 16000;
-		precos[3] = 55000;
-		precos[4] = 17000;
+		Produto[] produtos = new Produto[5];
+		produtos[0] = new Produto("Lamburguini", 1000000);
+		produtos[1] = new Produto("Gol", 46000);
+		produtos[2] = new Produto("Brasilia", 16000);
+		produtos[3] = new Produto("Fusca", 55000);
+		produtos[4] = new Produto("CLio", 7000);
 
 		int menorPreco = 0;
 
-		for (int atual = 0; atual < precos.length; atual++) {
-			if (precos[atual] < precos[menorPreco]) {
+		for (int atual = 0; atual < produtos.length; atual++) {
+			if (produtos[atual].getValor() < produtos[menorPreco].getValor()) {
 				menorPreco = atual;
 			}
 		}
 
 		System.out.println(menorPreco);
-		System.out.println(precos[menorPreco]);
+		System.out.println("O veiculo "+ produtos[menorPreco].getNome()+
+				", é o de menor valor, custando R$"+ produtos[menorPreco].getValor());
 	}
 
 }
---
+++ src/al/menorPreco/Produto.java
@@ -0,0 +1,21 @@
+package al.menorPreco;
+
+public class Produto {
+	
+	private String nome;
+	private double valor;
+	
+	public Produto(String nome, double valor) {
+		this.nome = nome;
+		this.valor = valor;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public double getValor() {
+		return valor;
+	}
+
+}
